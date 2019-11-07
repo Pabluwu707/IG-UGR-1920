00bec4941d3ba7f5358ebfa68bb6d63873932570
Galactic-O <pabloshiny@gmail.com>
2019-11-07 11:12:23 +0100

    Añadidos vectores de colores

--- cubo.cc
+++ cubo.cc
@@ -6,7 +6,7 @@ Cubo::Cubo(float lado)
 {
 
    // inicializar la tabla de vértices
-   v.resize(100);
+   v.resize(8);
    v[0] = Tupla3f (0,lado,lado); // Vértice 0
    v[1] = Tupla3f (lado,lado,lado); // Vértice 1
    v[2] = Tupla3f (0,0,lado); // Vértice 2
@@ -20,17 +20,29 @@ Cubo::Cubo(float lado)
    // inicializar la tabla de caras o triángulos:
    // (es importante en cada caerticesra ordenar los vértices en sentido contrario
    //  de las agujas del reloj, cuando esa cara se observa desde el exterior del cubo)
-   f.resize(100);
+   f.resize(12);
    f[0] = Tupla3i (2,1,0); // Triangulo 0
    f[1] = Tupla3i (1,2,3); // Triangulo 1
    f[2] = Tupla3i (3,5,1); // Triangulo 2
    f[3] = Tupla3i (3,7,5); // Triangulo 3
-   f[4] = Tupla3i (7,6,4); // Triangulo 4
-   f[5] = Tupla3i (7,4,5); // Triangulo 5
+   f[4] = Tupla3i (7,4,5); // Triangulo 5
+   f[5] = Tupla3i (7,6,4); // Triangulo 4
    f[6] = Tupla3i (6,0,4); // Triangulo 6
    f[7] = Tupla3i (2,0,6); // Triangulo 7
-   f[8] = Tupla3i (2,6,3); // Triangulo 8
-   f[9] = Tupla3i (3,6,7); // Triangulo 9
-   f[10] = Tupla3i (0,1,5); // Triangulo 10
-   f[11] = Tupla3i (0,5,4); // Triangulo 11
+   f[8] = Tupla3i (0,5,4); // Triangulo 11
+   f[9] = Tupla3i (0,1,5); // Triangulo 10
+   f[10] = Tupla3i (2,6,3); // Triangulo 8
+   f[11] = Tupla3i (3,6,7); // Triangulo 9
+
+   // Inicializar colores inmediato y diferido
+   for (int i=0; i<v.size(); i=i+2) {
+     c.push_back({1.0,0.0,0.64});
+     c.push_back({0.28,0.25,0.4});
+   }
+
+   // Inicializar colores inmediato y diferido
+   for (int i=0; i<v.size(); i++) {
+     cA1.push_back({1.0,0.0,0.64});
+     cA2.push_back({0.28,0.25,0.4});
+   }
 }
