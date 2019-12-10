8f461cce4586bbf7dd67f1e13fb153a04d308b3f
Galactic-O <pabloshiny@gmail.com>
2019-12-10 11:34:57 +0100

    Añadida inicializacion de colores

--- cubo.cc
+++ cubo.cc
@@ -34,5 +34,31 @@ Cubo::Cubo(float lado)
    f[10] = Tupla3i (2,6,3); // Triangulo 8
    f[11] = Tupla3i (3,6,7); // Triangulo 9
 
-   
+
+   // Inicializar colores puntos en Modo inmediato y diferido
+   cPuntos.clear();
+   for (int i=0; i<v.size(); i++) {
+     cPuntos.push_back({0.28,0.25,0.4});
+   }
+
+   // Inicializar colores lineas en Modo inmediato y diferido
+   cLineas.clear();
+   for (int i=0; i<v.size(); i++) {
+     cLineas.push_back({0.28,0.25,0.4});
+   }
+
+   // Inicializar colores solido para cubo en Modo inmediato y diferido
+   cSolido.clear();
+   for (int i=0; i<v.size(); i=i+2) {
+     cSolido.push_back({1.0,0.0,0.64});
+     cSolido.push_back({0.97,0.78,0.05});
+   }
+
+   // Inicializar colores cubo en Modo Ajedrez
+   cAjedrez1.clear();
+   cAjedrez2.clear();
+   for (int i=0; i<v.size(); i++) {
+     cAjedrez1.push_back({1.0,0.0,0.64});
+     cAjedrez2.push_back({0.97,0.78,0.05});
+   }
 }
