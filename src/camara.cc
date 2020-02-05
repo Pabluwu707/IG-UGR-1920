fcb80523004a9a8833c9843561d05228026b6755
Galactic-O <pabloshiny@gmail.com>
2020-02-05 12:23:57 +0100

    Correcciones menores en el código

--- src/camara.cc
+++ src/camara.cc
@@ -25,66 +25,68 @@ Camara::Camara(const Tupla3f eye, const Tupla3f at, const Tupla3f up,
 
 void Camara::rotarXExaminar(const float angle){
    eye = eye - at;
-   Tupla3f eyeOriginal = eye;
-   eye(1) = eyeOriginal(1) * cos(angle) - eyeOriginal(2) * sin(angle);
-   eye(2) = eyeOriginal(1) * sin(angle) + eyeOriginal(2) * cos(angle);
+   Tupla3f eye_antiguo = eye;
+   eye(1) = eye_antiguo(1) * cos(angle) - eye_antiguo(2) * sin(angle);
+   eye(2) = eye_antiguo(1) * sin(angle) + eye_antiguo(2) * cos(angle);
    eye = eye + at;
 }
 
 void Camara::rotarYExaminar(const float angle){
    eye = eye - at;
-   Tupla3f eyeOriginal = eye;
-   eye(0) = eyeOriginal(0) * cos(angle) + eyeOriginal(2) * sin(angle);
-   eye(2) = eyeOriginal(0)* -1 * sin(angle) + eyeOriginal(2) * cos(angle);
+   Tupla3f eye_antiguo = eye;
+   eye(0) = eye_antiguo(0) * cos(angle) + eye_antiguo(2) * sin(angle);
+   eye(2) = eye_antiguo(0)* -1 * sin(angle) + eye_antiguo(2) * cos(angle);
    eye = eye + at;
 }
 
 void Camara::rotarZExaminar(const float angle){
    eye = eye - at;
-   Tupla3f eyeOriginal = eye;
-   eye(0) = eyeOriginal(0) * cos(angle) - eyeOriginal(2) * sin(angle);
-   eye(1) = eyeOriginal(0) * sin(angle) + eyeOriginal(2) * cos(angle);
+   Tupla3f eye_antiguo = eye;
+   eye(0) = eye_antiguo(0) * cos(angle) - eye_antiguo(2) * sin(angle);
+   eye(1) = eye_antiguo(0) * sin(angle) + eye_antiguo(2) * cos(angle);
    eye = eye + at;
 }
 
 void Camara::rotarXFirstPerson(const float angle){
    at = at - eye;
-   Tupla3f atOriginal = at;
-   at(1) = atOriginal(1) * cos(angle) - atOriginal(2) * sin(angle);
-   at(2) = atOriginal(1) * sin(angle) + atOriginal(2) * cos(angle);
+   Tupla3f at_antiguo = at;
+   at(1) = at_antiguo(1) * cos(angle) - at_antiguo(2) * sin(angle);
+   at(2) = at_antiguo(1) * sin(angle) + at_antiguo(2) * cos(angle);
    at = at + eye;
 }
 void Camara::rotarYFirstPerson(const float angle){
    at = at - eye;
-   Tupla3f atOriginal = at;
-   at(0) = atOriginal(0) * cos(angle) + atOriginal(2) * sin(angle);
-   at(2) = atOriginal(0)* -1 * sin(angle) + atOriginal(2) * cos(angle);
+   Tupla3f at_antiguo = at;
+   at(0) = at_antiguo(0) * cos(angle) + at_antiguo(2) * sin(angle);
+   at(2) = at_antiguo(0)* -1 * sin(angle) + at_antiguo(2) * cos(angle);
    at = at + eye;
 }
 void Camara::rotarZFirstPerson(const float angle){
    at = at - eye;
-   Tupla3f atOriginal = at;
-   at(0) = atOriginal(0) * cos(angle) - atOriginal(2) * sin(angle);
-   at(1) = atOriginal(0) * sin(angle) + atOriginal(2) * cos(angle);
+   Tupla3f at_antiguo = at;
+   at(0) = at_antiguo(0) * cos(angle) - at_antiguo(2) * sin(angle);
+   at(1) = at_antiguo(0) * sin(angle) + at_antiguo(2) * cos(angle);
    at = at + eye;
 }
 
 // Mover el at y el eye de la cámara
 void Camara::mover(const float x, const float y, const float z){
-   at(0) = at(0)+x;
-   at(1) = at(1)+y;
-   at(2) = at(2)+z;
-   eye(0) = eye(0)+x;
-   eye(1) = eye(1)+y;
-   eye(2) = eye(2)+z;
+   at(0) += x;
+   at(1) += y;
+   at(2) += z;
+   eye(0) += x;
+   eye(1) += y;
+   eye(2) += z;
 }
 
+// Aplicar zoom aumentando o disminuyendo los marcos del frustum
 void Camara::zoom(const float factor){
-   right = right * factor;
-   left = left * factor;
-   top = top * factor;
-   bottom = bottom * factor;
+   left *= factor;
+   right *= factor;
+   top *= factor;
+   bottom *= factor;
 }
+
 void Camara::girar(const float x, const float y){
    rotarXFirstPerson(-y*0.25*M_PI/180.0);
    rotarYFirstPerson(-x*0.25*M_PI/180.0);
